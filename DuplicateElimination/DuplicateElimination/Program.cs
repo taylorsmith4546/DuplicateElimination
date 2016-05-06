d848e74a6d335716e87ba115a355a6ad47eb05f1
Taylor Smith <taylorsmith4546@gmail.com>
2016-05-06 10:28:48 -0700

    initial commit

---
+++ DuplicateElimination/DuplicateElimination/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateElimination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            bool numberIn = false;
+            int counter = 0;
+            int[] numbers = new int[5];
+
+            while (counter < 5)
+            {
+                Console.WriteLine("Please Enter a Number Between 10 and 100");
+            }
+
+        }
+    }
+}
